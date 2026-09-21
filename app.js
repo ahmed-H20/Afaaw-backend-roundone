1c8df8b93d0bb3ca3d816c35074de297a5297ee3
Hanan <Hanan.bayazeed56@gmail.com>
2026-09-21 15:20:25 +0300

    feat(user): implement user CRUD

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const productRoute = require("./routes/productRoute");
-
+const userRoute = require("./routes/userRoutes");
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+  res.send("Hello, World!");
 });
 
 app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
 
 module.exports = app;
